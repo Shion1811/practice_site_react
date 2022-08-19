9caf4f1f600a9ace56624cb9697b28844054a9d8
shion <shion48691811@gmail.com>
2022-08-20 03:06:26 +0900

    training.html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <h>aiueo</h>
-  </React.StrictMode>
+  <>
+    <script>
+      <h1>趣味の部屋</h1>
+      <div className="header">
+        <a href="apple.html">apple</a>
+        <a href="parakeet.html">インコ</a>
+        <a href="SEKAI NO OWARI.html">SEKAI NO OWARI</a>
+      </div>
+      <div class="main">
+        <p class="april_11_2020">
+          <img src="1.JPG" alt="なーちゃんお迎え日" width="500" height="500" />
+        </p>
+      </div>
+    </script>
+  </>
 );
